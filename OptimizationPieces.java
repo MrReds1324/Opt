0f63876d9747decabda49ab51c6e1d532deff688
Ryan Boyles <ryanb27@gmail.com>
2018-10-29 23:18:01 -0400

    Initial Commit

---
+++ OptimizationPieces.java
@@ -0,0 +1,60 @@
+package optimizationpieces;
+
+public class OptimizationPieces extends javax.swing.JFrame{
+
+
+    private void bpActionPerformed_FormGen(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bpActionPerformed_FormGen
+        bpActionPerformed(evt);
+    }//GEN-LAST:event_bpActionPerformed_FormGen
+
+
+    private void secbpSelectActionPerformed_FormGen(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_secbpSelectActionPerformed_FormGen
+        secbpSelectActionPerformed(evt);
+    }//GEN-LAST:event_secbpSelectActionPerformed_FormGen
+
+
+    private void secSelectActionPerformed_FormGen(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_secSelectActionPerformed_FormGen
+        secSelectActionPerformed(evt);
+    }//GEN-LAST:event_secSelectActionPerformed_FormGen
+
+
+    private void wepNebSelectActionPerformed_FormGen(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_wepNebSelectActionPerformed_FormGen
+        wepNebSelectActionPerformed(evt);
+    }//GEN-LAST:event_wepNebSelectActionPerformed_FormGen
+
+
+    private void secNebSelectActionPerformed_FormGen(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_secNebSelectActionPerformed_FormGen
+        secNebSelectActionPerformed(evt);
+    }//GEN-LAST:event_secNebSelectActionPerformed_FormGen
+
+
+    private void kannaClassActionPerformed_FormGen(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_kannaClassActionPerformed_FormGen
+        kannaClassActionPerformed(evt);
+    }//GEN-LAST:event_kannaClassActionPerformed_FormGen
+
+
+    private void zeroClassActionPerformed_FormGen(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_zeroClassActionPerformed_FormGen
+        zeroClassActionPerformed(evt);
+    }//GEN-LAST:event_zeroClassActionPerformed_FormGen
+
+
+    private void zeroClassStateChanged_FormGen(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_zeroClassStateChanged_FormGen
+        zeroClassStateChanged(evt);
+    }//GEN-LAST:event_zeroClassStateChanged_FormGen
+
+
+	public OptimizationPieces() {
+		initComponents();
+	}
+
+private void initComponents() {//GEN-BEGIN:initComponents
+
+}//GEN-END:initComponents
+
+//GEN-BEGIN:variables
+
+//GEN-END:variables
+
+}
+
+
---
+++ src/optimizationpieces/Union.java
@@ -0,0 +1,68 @@
+package optimizationpieces;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author ryanb
+ */
+public class Union {
+
+     private final int uied;
+    private final int ub;
+    //Double values of each potential
+    public double vals = 0.01; //values used for ied and nebs
+
+    //Constructor for Nebs_U object (val of ied neb, val of boss neb, val or union ied, value of union boss, number of ied nebs, number of boss nebs)
+    Union(int uied, int ub) {
+        this.uied = uied;      //val or union ied
+        this.ub = ub;    //val or union boss
+    }
+
+    //Calculates the ied added by the union and nebulites
+    public double cied() {
+            return (1 - (this.uied * vals));
+            }
+
+    //Calculates the boss added by the union and nebulites
+    public double cboss() {
+        return (this.ub * vals);
+    }
+
+    //The Equals Method of this class, if the values of the first 4 entries in each objects nebsu are the same then they are equal
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Union)) {
+            return false;
+        } else {
+            Union p = (Union) o;
+            return (this.uied == p.uied) && (this.ub == p.ub);
+        }
+    }
+
+    //The hashCode for this class, hashs on our nebsu array as this is the only variable that may change
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.uied;
+        hash = 97 * hash + this.ub;
+        return hash;
+    }
+
+   
+    public String legionString() {
+        String s = this.uied + "% IED\n";
+        s += this.ub + "% BOSS";
+        return s;
+    }
+
+    //ToString method for this class
+    @Override
+    public String toString() {
+        String s = "";
+        s += "Legion System:\n";
+        s += "IED Points: " + this.uied + "\n";
+        s += "BOSS Points: " + this.ub + "\n";
+        return s;
+    }
+}
