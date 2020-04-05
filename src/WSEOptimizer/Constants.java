b66526dd262b94977d493967b9e7c83b02cdb00f
MrReds1324 <ryanb27@gmail.com>
2020-04-05 17:48:16 -0400

    Added individual hyper stat values

--- src/WSEOptimizer/Constants.java
+++ src/WSEOptimizer/Constants.java
@@ -89,6 +89,11 @@ public class Constants {
     
     public static final PotType[] souls = new PotType[]{PotType.ATT, PotType.BOSS, PotType.IED};
     
+    // Stats for Hyper Stats
     public static final int[] hyperStatCosts = new int[]{0, 1, 3, 7, 15, 25, 40, 60, 85, 115, 150, 200, 265, 345, 440, 550};
-    
+    public static final double[] hyperCritDmg = new double[]{0.0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.13, 0.14, 0.15};
+    public static final double[] hyperBossDmg = new double[]{0.0, 0.03, 0.06, 0.09, 0.12, 0.15, 0.19, 0.23, 0.27, 0.31, 0.35, 0.39, 0.43, 0.47, 0.51, 0.55};
+    public static final double[] hyperDmg = new double[]{0.0, 0.03, 0.06, 0.09, 0.12, 0.15, 0.18, 0.21, 0.24, 0.27, 0.30, 0.33, 0.36, 0.39, 0.42, 0.45};
+    public static final double[] hyperIed = new double[]{0.0, 0.03, 0.06, 0.09, 0.12, 0.15, 0.18, 0.21, 0.24, 0.27, 0.30, 0.33, 0.36, 0.39, 0.42, 0.45};
+
 }
