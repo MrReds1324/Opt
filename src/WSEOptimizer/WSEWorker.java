e00cdad19825114c89e456e747b9f3da429c175a
MrReds1324 <ryanb27@gmail.com>
2020-04-11 05:31:14 -0400

    Added basic non-reboot optimization to the worker thread

--- src/WSEOptimizer/WSEWorker.java
+++ src/WSEOptimizer/WSEWorker.java
@@ -70,54 +70,113 @@ public class WSEWorker extends SwingWorker<ArrayList<PotVector>, ArrayList<PotVe
 
     @Override
     protected ArrayList<PotVector> doInBackground() throws Exception {
-        ArrayList<WSEOptimizationThread> threads = new ArrayList();
-        ArrayList<PotVector> potVectorList = new ArrayList();
-        //Carries out the optimization beginning with Emblem to find the perfect configuration
-        for (int[] hyper: hyperStats){
-            threads.add(new WSEOptimizationThread(hyper, legion, weapon, secondary, emblem, souls, classType, sw_abs, sec160, Server.REBOOT));
-//            for (PotType soul : souls){
-//                for (PotType[] emb : emblem) {
-//                    //Saves the potentials and then checks if they are feasible, If they are go to the next piece of gear, else go to the next potential combination
-//                    Potentials etemp = new Potentials(emb[0], emb[1], emb[2], false);
-//                    for (PotType[] wep : weapon) {
-//                        //Saves the potentials and then checks if they are feasible, If they are go to the next piece of gear, else go to the next potential combination
-//                        Potentials wtemp = new Potentials(wep[0], wep[1], wep[2], sw_abs);
-//                        switch (classType) {
-//                            case ZERO:
-//                                potVectorList = legionAndAddReduce(potVectorList, wtemp, wtemp, etemp, null, null, null, hyper, soul);
-//                                break;
-//                            case KANNA:
-//                                //Secondary fan only recognizes Magic Att%
-//                                Potentials stemp = new Potentials(secondary[0][0], secondary[0][1], secondary[0][2], sec160);
-//                                potVectorList = legionAndAddReduce(potVectorList, wtemp, stemp, etemp, null, null, null, hyper, soul);
-//                                break;
-//                            default:
-//                                for (PotType[] sec : secondary) {
-//                                    //Saves the potentials and then checks if they are feasible, If they are calculate the multiplier, else go to the next potential combination
-//                                    stemp = new Potentials(sec[0], sec[1], sec[2], sec160);
-//                                    potVectorList = legionAndAddReduce(potVectorList, wtemp, stemp, etemp, null, null, null, hyper, soul);
-//                                }
-//                                break;
-//                        }
-//                    }
-//                }
-//            }
-//            System.out.println(String.format("%.3f%% Completed", (double)counter/totalGenerationSpace * 100));
-        }
-        System.out.println();
-        for (WSEOptimizationThread thread : threads){
-            thread.start();
-        }
-        for (WSEOptimizationThread thread : threads){
-            try {
-                thread.join();
-                potVectorList.addAll(thread.getPotVectors());
-            } catch (InterruptedException ex) {
-                System.out.println(ex.toString());
+        switch(server){
+            case REBOOT:
+                ArrayList<WSEOptimizationThread> threads = new ArrayList();
+                ArrayList<PotVector> potVectorList = new ArrayList();
+                //Carries out the optimization beginning with Emblem to find the perfect configuration
+                for (int[] hyper: hyperStats){
+                    threads.add(new WSEOptimizationThread(hyper, legion, weapon, secondary, emblem, souls, classType, baseDMG, baseBOSS, baseATT, baseIED, baseCRIT, PDR, sw_abs, sec160, options, Server.REBOOT));
+                }
+                System.out.println();
+                for (WSEOptimizationThread thread : threads){
+                    thread.start();
+                }
+                for (WSEOptimizationThread thread : threads){
+                    try {
+                        thread.join();
+                        potVectorList.addAll(thread.getPotVectors());
+                    } catch (InterruptedException ex) {
+                        System.out.println(ex.toString());
+                    }
+                }
+                setProgress(100);
+                return WSEHelpers.reduce(potVectorList, options);
+            case NONREBOOT:
+                ArrayList<PotVector> main_temp = new ArrayList();
+                ArrayList<PotVector> bonus_temp = new ArrayList();
+                potVectorList = new ArrayList();
+
+                for (PotType[] emb : emblemBp) {
+                    //Saves the potentials and then checks if they are feasible, If they are go to the next piece of gear, else go to the next potential combination
+                    Potentials etempb = new Potentials(emb[0], emb[1], emb[2], false, true);
+                    for (PotType[] wep : weaponBp) {
+                        //Saves the potentials and then checks if they are feasible, If they are go to the next piece of gear, else go to the next potential combination
+                        Potentials wtempb = new Potentials(wep[0], wep[1], wep[2], sw_abs, true);
+                        switch (classType){
+                            case ZERO:
+                                //Add the potVector to the list
+                                PotVector ptb = new PotVector(wtempb, wtempb, etempb, null, null, null);
+                                //Add the configuration to the WSE array if it does not exist
+                                bonus_temp.add(ptb);
+                                break;
+                            case KANNA:
+                                //Secondary fan only recognizes Magic Att%
+                                Potentials stempb = new Potentials(secondaryBp[0][0], secondaryBp[0][1], secondaryBp[0][2], sec160, true);
+                                //Add the potVector to the list
+                                ptb = new PotVector(wtempb, stempb, etempb, null, null, null);
+                                //Add the configuration to the WSE array if it does not exist
+                                bonus_temp.add(ptb);
+                                break;
+                            default:
+                                for (PotType[] sec : secondaryBp) {
+                                    //Saves the potentials and then checks if they are feasible, If they are calculate the multiplier, else go to the next potential combination
+                                    stempb = new Potentials(sec[0], sec[1], sec[2], sec160, true);
+                                    //Add the potVector to the list
+                                    ptb = new PotVector(wtempb, stempb, etempb, null, null, null);
+                                    //Add the configuration to the WSE array if it does not exist
+                                    bonus_temp.add(ptb);
+                                }
+                                break;
+                        }
+                    }
+                }
+                //Carries out the optimization beginning with Emblem to find the perfect configuration
+                for (PotType[] emb : emblem) {
+                    //Saves the potentials and then checks if they are feasible, If they are go to the next piece of gear, else go to the next potential combination
+                    Potentials etemp = new Potentials(emb[0], emb[1], emb[2], false);
+                    for (PotType[] wep : weapon) {
+                        //Saves the potentials and then checks if they are feasible, If they are go to the next piece of gear, else go to the next potential combination
+                        Potentials wtemp = new Potentials(wep[0], wep[1], wep[2], sw_abs);
+                        switch (classType) {
+                            case ZERO:
+                                main_temp.add(new PotVector(wtemp, wtemp, etemp, new int[]{0, 0, 0}, new int[]{0, 0, 0, 0}, PotType.DEFAULT));
+                                break;
+                            case KANNA:
+                                //Secondary fan only recognizes Magic Att%
+                                Potentials stemp = new Potentials(secondary[0][0], secondary[0][1], secondary[0][2], sec160);
+                                main_temp.add(new PotVector(wtemp, stemp, etemp, new int[]{0, 0, 0}, new int[]{0, 0, 0, 0}, PotType.DEFAULT));
+                                break;
+                            default:
+                                for (PotType[] sec : secondary) {
+                                    //Saves the potentials and then checks if they are feasible, If they are calculate the multiplier, else go to the next potential combination
+                                    stemp = new Potentials(sec[0], sec[1], sec[2], sec160);
+                                    main_temp.add(new PotVector(wtemp, stemp, etemp, new int[]{0, 0, 0}, new int[]{0, 0, 0, 0}, PotType.DEFAULT));
+                                }
+                                break;
+                        }
+                    }
+                }
+
+                threads = new ArrayList();
+                long totalGenerationSpace = hyperStats.size() * souls.length * emblem.length * weapon.length * secondary.length * emblemBp.length * weaponBp.length * secondaryBp.length * lcombs.size();
+                //Combines both main and bonus pots to generate all combinations of the two
+                for (int[] hyper : hyperStats){
+                    threads.add(new WSEOptimizationThread(hyper, legion, main_temp, bonus_temp, souls, baseDMG, baseBOSS, baseATT, baseIED, baseCRIT, PDR, options, Server.NONREBOOT));
+                }
+                for (WSEOptimizationThread thread : threads){
+                    thread.start();
+                }
+                for (WSEOptimizationThread thread : threads){
+                    try {
+                        thread.join();
+                        potVectorList.addAll(thread.getPotVectors());
+                    } catch (InterruptedException ex) {
+                        System.out.println(ex.toString());
+                    }
+                }
+                return WSEHelpers.reduce(potVectorList, options);
             }
+         return null;
         }
-        setProgress(100);
-        return WSEHelpers.reduce(potVectorList, options);
     }
-       
-}
